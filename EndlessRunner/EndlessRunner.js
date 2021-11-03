6dff4349f33aadea688e59d58474f45723fc3493
Pirasp <johanneshofer@gmx.net>
2021-11-03 10:18:57 +0100

    added javascript file and automatic canvas resizer

---
+++ EndlessRunner/EndlessRunner.js
@@ -0,0 +1,16 @@
+//the javascript component for the endless runner
+//Written by Johannes Hofer
+//https://github.com/Pirasp
+//johanneshofer@gmx.net
+
+
+    var Canvas = document.getElementById("EndlessRunnerCanvas");
+    rescaleCanvas();
+    window.onresize = rescaleCanvas;
+
+    function rescaleCanvas(){
+        Canvas.height = window.innerHeight;
+        Canvas.width = window.innerWidth;
+    }
+
+    
