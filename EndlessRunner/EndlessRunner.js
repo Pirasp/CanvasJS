f095c5bab8b8d37da7c2527a566a4f00e70bea3d
Pirasp <johanneshofer@gmx.net>
2021-11-05 21:00:21 +0100

    started EndlessRunner scene setup

--- EndlessRunner/EndlessRunner.js
+++ EndlessRunner/EndlessRunner.js
@@ -6,13 +6,21 @@
 
     var Canvas = document.getElementById("EndlessRunnerCanvas");
     rescaleCanvas();
-    window.onresize = rescaleCanvas;
 
     function rescaleCanvas(){
         Canvas.height = window.innerHeight;
         Canvas.width = window.innerWidth;
     }
 
-    
+    var c = Canvas.getContext('2d');
 
-    
+    c.fillStyle='lightgray';
+    c.fillRect(0, window.innerHeight/2, window.innerWidth, window.innerHeight/2);
+
+    function animate(){
+        //clears the upper halve of the screen so the ground only needs to be drawn once
+        c.clearRect(0, 0, window.innerWidth, window.innerHeight/2);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
