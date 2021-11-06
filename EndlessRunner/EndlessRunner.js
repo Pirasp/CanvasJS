c2d8ae6b1361bafbba3c584f0d833c71fec63823
Pirasp <johanneshofer@gmx.net>
2021-11-06 19:13:36 +0100

    started player character, jumping not working yet

--- EndlessRunner/EndlessRunner.js
+++ EndlessRunner/EndlessRunner.js
@@ -14,12 +14,52 @@
 
     var c = Canvas.getContext('2d');
 
+    //dwar ground
     c.fillStyle='lightgray';
     c.fillRect(0, window.innerHeight/2, window.innerWidth, window.innerHeight/2);
+
+    function Player(gravity, jumpvelocity, playerheight){
+        var grav = gravity; var impulse = jumpvelocity; var size = playerheight;
+        var defaultY = window.innerHeight/2-playerheight;
+        var y = defaultY; var yvel = 0;
+
+
+        this.update = function(){
+
+            //jumping and gravity
+            if(this.y < this.defaultY){
+
+                this.y += this.yvel;
+                this.yvel -= this.grav;
+
+            }
+            //character ground collision
+            if(this.y >= this.defaultY){
+                this.y = this.defaultY; 
+                this.yvel = 0;
+            }
+            //character jump input
+            //TODO: keycode abfrage im IF
+            if(this.y >= this.defaultY){
+
+                this.yvel = this.jumpvelocity;
+
+            }
+
+            //draw character
+            c.fillStyle = 'blue';
+            c.fillRect(250, y, 20, size);
+
+        }
+
+    }
+
+    var char = new Player(0.1, 10, 40);
 
     function animate(){
         //clears the upper halve of the screen so the ground only needs to be drawn once
         c.clearRect(0, 0, window.innerWidth, window.innerHeight/2);
+        char.update();
         requestAnimationFrame(animate);
     }
 
